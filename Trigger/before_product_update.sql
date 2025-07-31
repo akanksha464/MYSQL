24feeb336c0c8ba3a6df3ce75b802fd141712d6a
akanksha464 <akshuwayal9804@gmail.com>
2025-07-31 20:29:06 +0530

    new file inserted

---
+++ Trigger/before_product_update.sql
@@ -0,0 +1,13 @@
+-- rigger to Update the Last Modified Date on Product Changes
+
+DELIMITER //
+
+CREATE TRIGGER before_product_update
+BEFORE UPDATE ON products
+FOR EACH ROW
+BEGIN
+    -- Set the last_modified field to the current timestamp
+    SET NEW.last_modified = NOW();
+END//
+
+DELIMITER ;
